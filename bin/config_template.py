0da188274266f5b5bb73f81ad1f7a606d41a5f1c
Kamil Madac <kamil.madac@gmail.com>
2013-07-15 13:04:29 +0200

    scripts and config added to repo

---
+++ bin/config_template.py
@@ -0,0 +1,13 @@
+__author__ = 'kmadac'
+
+#Path to directory where public keys are stored
+keypath = '/home/kmadac/PycharmProjects/NetappUserManagement/test/keys'
+
+#Path to hosts file (/etc/hosts normally)
+hosts_file = '/home/kmadac/PycharmProjects/NetappUserManagement/test/hosts'
+
+#identification string of filers in hosts file
+hosts_filter = ['#fn', '#fc']
+
+#root password for filers in environment.
+root_passwords = ['pass1', 'pass2', 'pass...']
